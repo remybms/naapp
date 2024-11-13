7aa0e6109dc0aba4e22421e885959edf08569911
remybms <bamasremy43@gmail.com>
2024-11-13 13:59:08 +0100

    edit website structure to add correctly the header and footer

--- app/community/page.js
+++ app/community/page.js
@@ -1,7 +1,14 @@
-export default function Community(){
-    return(
-        <main>
-            Communauté
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+
+export default function Community() {
+    return (
+        <main className="flex flex-col min-h-screen justify-between items-center">
+            <Header />
+            <div>
+                Communauté
+            </div>
+            <Footer />
         </main>
     )
 }
--- app/neuroatypie/page.js
+++ app/neuroatypie/page.js
@@ -1,7 +1,14 @@
-export default function Neuroatypie(){
-    return(
-        <main>
-            Liste des troubles
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+
+export default function Neuroatypie() {
+    return (
+        <main className="flex flex-col min-h-screen justify-between items-center">
+            <Header />
+            <div>
+                Liste des troubles
+            </div>
+            <Footer />
         </main>
     )
 }
