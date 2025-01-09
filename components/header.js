51ab4948b6e7bb898e8e4af4a28ccb85ea197d36
remybms <bamasremy43@gmail.com>
2025-01-09 09:26:37 +0100

    start creating header

--- components/header.js
+++ components/header.js
@@ -1,7 +1,16 @@
+import Hamburger from './hamburger'
+import Link from 'next/link'
+
 export default function Header(){
     return (
-        <div>
+        <>
+        <div className="hidden tablet:flex">
             header
         </div>
+        <div className="flex tablet:hidden bg-gradient-to-r from-blue to-violet w-screen h-24 items-center justify-between">
+            <Link href="/"><img src="/img/LOGO_BLANC.png" className=" mt-4 ml-4 h-12" /></Link>
+            <Hamburger />
+        </div>
+        </>
     )
 }
